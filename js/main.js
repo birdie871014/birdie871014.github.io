c02d3eb4506453f196d3b70aeab538679ee5c2f4
birdie <birdie.lipot@gmail.com>
2022-06-03 10:54:12 +0200

    0.0.3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-//Send an idea
 
+// send an idea to local json server
+
+
+function sendIdea() {
+    let idea = document.querySelector("#idea").value;
+    let data = { "text": idea }
+    let fetchOptions = {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    }
+    fetch("http://localhost:3000/ideas", fetchOptions).then(
+        resp => resp.json(),
+        err => console.error(err)
+    ).then(
+        data => console.log(data),
+        alert("Köszönjöm az észrevételt!")
+    )
+
+}
+
+
+//Send an idea to jsonbin.io
 
 function sendIdea1() {
     let req = new XMLHttpRequest();
@@ -12,34 +38,11 @@ function sendIdea1() {
 
     let idea = document.querySelector("#idea").value;
     let data = { "text": idea }
-    req.open("POST", "https://api.jsonbin.io/b", true);
+    req.open("POST", "https://api.jsonbin.io/v3/b", true);
     req.setRequestHeader("Content-Type", "application/json");
-    req.setRequestHeader("X-Master-Key", "$2b$10$hp9/wd9NCSdkeHRwL957IeR8SWdOSO06ciao741IB3uwcv7nN3DpC");
+    req.setRequestHeader("X-Master-key", "$2b$10$JD2kC0Ju32K8zhFDWPKc6uGVvBLtBgGGVEfJHaPptIvFuVp0ShETC");
     req.send('{"sample": "Hello World"}');
     console.log(data)
 }
 
 
-
-function sendIdea2() {
-    let idea = document.querySelector("#idea").value;
-    let data = { "text": idea }
-    let fetchOptions = {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Master-Key': '$2b$10$JD2kC0Ju32K8zhFDWPKc6uGVvBLtBgGGVEfJHaPptIvFuVp0ShETC',
-        },
-        body: JSON.stringify(data)
-    }
-    fetch(`https://api.jsonbin.io/b`, fetchOptions).then(
-        resp => resp.json(),
-        err => console.error(err)
-    ).then(
-        data => console.log(data),
-        alert("Köszönjöm az észrevételt!")
-    )
-
-}
